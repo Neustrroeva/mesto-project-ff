58b62968df3964be4ba72691d1ce65a5a4912766
Elena Neustroeva <Neustrroeva@yandex.ru>
2025-05-05 02:22:52 +0500

    Исправленная ПР-5

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const cardsList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard(name, link, deleteCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle= cardElement.querySelector('.card__title');
     const deleteButton = cardElement.querySelector('.card__delete-button');
     cardImage.src = link;
-    cardTitle.alt = name;
+    cardImage.alt = name;
     cardTitle.textContent = name;
 
     deleteButton.addEventListener('click', () => {
